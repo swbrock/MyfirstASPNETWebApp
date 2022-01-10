dc9abc4699ed695fecec8b40093c107b88a6b0e1
swbrock <90523707+swbrock@users.noreply.github.com>
2022-01-10 10:40:24 -0700

    Add project files.

---
+++ MyfirstASPNETWebApp/wwwroot/JS/FanMail.js
@@ -0,0 +1,7 @@
+﻿$("#btnsend").click(function () {
+    alert("From: " + $("#txtfrom").val() +
+        ", Subject: " + $("#txtsubject").val() +
+        ", Message: " + $("#txtmessage").val());
+
+    $("#picGanderson").fadeOut("slow");
+})
